490df9c6fc982a92173a9d890477b260ba26d62a
William Knight <William.Jeffery.Knight@protonmail.com>
2024-03-29 10:57:40 -0400

    gainXP can change the points. Than the level of player. Reset the player experience points for that level. Each level has max point 10.

--- oop-game-challenge.js
+++ oop-game-challenge.js
@@ -4,16 +4,26 @@ let Player = function (name) {
   this.points = 0;
 };
 
-Player.prototype.gainXP = function (num){
-    this.points += num
-}
+Player.prototype.gainXP = function (num) {
+  this.points += num;
 
+  if (this.points >= 10) {
+    this.lvl++;
+    this.points = this.points - 10;
+  }
+};
 
 let player1 = new Player("Bob");
 let player2 = new Player("Alice");
 
-player1.gainXP(5)
-player2.gainXP(15)
+player1.gainXP(5);
+player2.gainXP(15);
+player1.gainXP(5);
+player2.gainXP(5);
+player1.gainXP(5);
+player2.gainXP(15);
+player1.gainXP(5);
+player2.gainXP(15);
 
 console.log(player1);
 console.log(player2);
