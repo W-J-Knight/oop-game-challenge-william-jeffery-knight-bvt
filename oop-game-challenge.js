b87376c0181e93f5ad18b3ee0695eb7b232888c5
William Knight <William.Jeffery.Knight@protonmail.com>
2024-03-29 10:50:19 -0400

    Create a method call gainXp that can add to the players points

--- oop-game-challenge.js
+++ oop-game-challenge.js
@@ -1,12 +1,20 @@
-let Player = function (name){
-    this.name = name;
-    this.lvl = 1;
-    this.points = 0
+let Player = function (name) {
+  this.name = name;
+  this.lvl = 1;
+  this.points = 0;
+};
+
+Player.prototype.gainXP = function (num){
+    this.points += num
 }
 
-let player1= new Player("Bob")
 
+let player1 = new Player("Bob");
+let player2 = new Player("Alice");
 
+player1.gainXP(5)
+player2.gainXP(15)
 
-
-console.log(player1)
+console.log(player1);
+console.log(player2);
+// console.log(Player.gainXP)
